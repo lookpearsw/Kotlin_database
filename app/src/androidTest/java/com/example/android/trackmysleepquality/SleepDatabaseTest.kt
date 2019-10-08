1affa3dbc4ff37d1c4f3a66c209f3810d4a1b649
lookpearsw <supajeew@gmail.com>
2019-10-08 15:25:35 +0700

    Add a ViewModel

--- app/src/androidTest/java/com/example/android/trackmysleepquality/SleepDatabaseTest.kt
+++ app/src/androidTest/java/com/example/android/trackmysleepquality/SleepDatabaseTest.kt
@@ -14,12 +14,6 @@ import org.junit.runner.RunWith
 import java.io.IOException
 
 
-/**
- * This is not meant to be a full set of tests. For simplicity, most of your samples do not
- * include tests. However, when building the Room, it is helpful to make sure it works before
- * adding the UI.
- */
-
 @RunWith(AndroidJUnit4::class)
 class SleepDatabaseTest {
 
